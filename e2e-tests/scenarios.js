cf91e051b9eaf4cbc25955b9e165db52c9501cd6
Maksim1Sheina <hai_power@mail.ru>
2017-02-07 14:49:31 +0300

    Step_5

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -1,12 +1,30 @@
-'use strict';
+//'use strict';
 
 // Angular E2E Testing Guide:
 // https://docs.angularjs.org/guide/e2e-testing
 
-describe('My app', function() {
-
-  beforeEach(function() {
-    browser.get('index.html');
-  });
-
+describe('PhoneCat Application', function() {
+    
+    describe('phoneList', function(){
+         
+        beforeEach(function(){
+             browser.get('index.html');
+        });
+        
+        it('should filter the phone list as user types into the search box', function(){
+            var phoneList = element.all(by.repeater('phone in $ctrl.phones'));
+            var query = element(by.model('$ctrl.query'));
+            
+            expect(phoneList.count()).toBe(4);
+            
+            query.sendKeys('nexus');
+            expect(phoneList.count()).toBe(1);
+            
+            query.clear();
+            query.sendKeys('nokia');
+            expect(phoneList.count()).toBe(2);
+        });
+        
+    });
+    
 });
