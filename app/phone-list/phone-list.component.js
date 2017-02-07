4f6361b958612c5b290e754912a4898d0f02e037
Maksim1Sheina <hai_power@mail.ru>
2017-02-07 15:14:53 +0300

    Step_6(part of it)

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -7,20 +7,26 @@ angular.
                 {
                   name: 'Nexus S',
                   snippet: 'Fast just got faster with Nexus S.',
-                  mes: ', people!'
+                  mes: ', people!',
+                  age: 4
                 }, {
                   name: 'Motorola XOOM™ with Wi-Fi',
                   snippet: 'The Next, Next Generation tablet.',
-                  mes: '. And nothing more.'
+                  mes: '. And nothing more.',
+                  age: 2
                 }, {
                   name: 'Nokia Lumia',
                   snippet: 'The Next, Next Generation tablet.',
-                  mes: 'all. Nokia - connecting people'
+                  mes: 'all. Nokia - connecting people',
+                  age: 3
                 }, {
                   name: 'Nokia 3310',
                   snippet: 'Perfect instrument for breaking briks and nailing',
-                  mes: 'all. Nokia - connecting all in your house =)'
+                  mes: 'all. Nokia - connecting all in your house =)',
+                  age: 1
                 }
             ];
+            
+        this.orderProp = 'age';
     }
 });
