eb20a0bdbb9e3a3870e578da0ada3badd39e791a
Maksim1Sheina <hai_power@mail.ru>
2017-02-09 15:14:32 +0300

    Step_8

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -6,7 +6,7 @@ describe('PhoneCat Application', function() {
              browser.get('index.html');
         });
         
-        it('should filter the phone list as user types into the search box', function(){
+        /*it('should filter the phone list as user types into the search box', function(){
             var phoneList = element.all(by.repeater('phone in $ctrl.phones'));
             var query = element(by.model('$ctrl.query'));
             
@@ -18,6 +18,14 @@ describe('PhoneCat Application', function() {
             query.clear();
             query.sendKeys('nokia');
             expect(phoneList.count()).toBe(2);
+        });*/
+        
+        it('should render phone specific links', function(){
+            var query = element(by.model('$ctrl.query'));
+            query.sendKeys('nexus');
+            
+            element.all(by.css('.phones li a')).first().click();
+            expect(browser.getLocationAbsUrl()).toBe('/phones/nexus-s');
         });
         
         it('should be possible to control phone order via the drop-down menu', function() {
@@ -35,15 +43,15 @@ describe('PhoneCat Application', function() {
           queryField.sendKeys('tablet');   // Let's narrow the dataset to make the assertions shorter
 
           expect(getNames()).toEqual([
-            'Nokia Lumia',
-            'Motorola XOOM\u2122 with Wi-Fi'
+            'Motorola XOOM\u2122 with Wi-Fi',
+            'MOTOROLA XOOM\u2122'
           ]);
 
           nameOption.click();
             
           expect(getNames()).toEqual([
-            'Motorola XOOM\u2122 with Wi-Fi',
-            'Nokia Lumia'
+            'MOTOROLA XOOM\u2122',
+            'Motorola XOOM\u2122 with Wi-Fi'
           ]);
             
         });
