13e8f8c74fca99c2562b245df588bee202093a82
Maksim1Sheina <hai_power@mail.ru>
2017-02-09 14:47:59 +0300

    Step_7

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -2,31 +2,16 @@ angular.
     module('phoneList').
     component('phoneList', {
         templateUrl: 'phone-list/phone-list.template.html',
-        controller: function PhoneListController(){
-            this.phones = [
-                {
-                  name: 'Nexus S',
-                  snippet: 'Fast just got faster with Nexus S.',
-                  mes: ', people!',
-                  age: 1
-                }, {
-                  name: 'Motorola XOOM™ with Wi-Fi',
-                  snippet: 'The Next, Next Generation tablet.',
-                  mes: '. And nothing more.',
-                  age: 3
-                }, {
-                  name: 'Nokia Lumia',
-                  snippet: 'The Next, Next Generation tablet.',
-                  mes: 'all. Nokia - connecting people',
-                  age: 2
-                }, {
-                  name: 'Nokia 3310',
-                  snippet: 'Perfect instrument for breaking briks and nailing',
-                  mes: 'all. Nokia - connecting all in your house =)',
-                  age: 4
-                }
-            ];
+        controller: ['$http', function PhoneListController($http){
+            var self = this;
+            self.orderProp = 'age';
             
-        this.orderProp = 'age';
-    }
+            $http.get('phones/phones.json').then(function(response){
+                self.phones = response.data;
+                
+                // The following code get from JSON file only 5 fisrt records.
+                //self.phones = response.data.slice(0,5);
+            });
+        }
+    ]
 });
