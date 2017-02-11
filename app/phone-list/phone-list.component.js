b62f30cbc2b17f6be92e1f0f2ae06a8e55eb00e6
Maksim1Sheina <hai_power@mail.ru>
2017-02-11 13:27:20 +0300

    Step_13

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -4,16 +4,10 @@ angular.
     module('phoneList').
     component('phoneList', {
         templateUrl: 'phone-list/phone-list.template.html',
-        controller: ['$http', function PhoneListController($http){
-            var self = this;
-            self.orderProp = 'age';
-            
-            $http.get('phones/phones.json').then(function(response){
-                self.phones = response.data;
-                
-                // The following code get from JSON file only 5 fisrt records.
-                //self.phones = response.data.slice(0,5);
-            });
-        }
-    ]
+        controller: ['Phone',
+            function PhoneListController(Phone){
+                this.phones = Phone.query();
+                this.orderProp = 'age';
+            }
+        ]
 });
--- app/phone-list/phone-list.component.spec.js
+++ app/phone-list/phone-list.component.spec.js
@@ -19,7 +19,9 @@ describe('phoneList', function(){
         }));
 
         it('should create a `phones` property with 2 phones fetched with `$http`', function() {
-            expect(ctrl.phones).toBeUndefined();
+            jasmine.addCustomEqualityTester(angular.equals);
+
+            expect(ctrl.phones).toEqual([]);
             
             $httpBackend.flush();
             expect(ctrl.phones).toEqual([
